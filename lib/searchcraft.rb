3184daed2cc8a45ebf2eb9e48faf4e4f1f48d4cb
Dr Nic Williams <drnicwilliams@gmail.com>
2023-08-19 21:39:28 +1000

    Include dependencies for scenic gem

--- lib/searchcraft.rb
+++ lib/searchcraft.rb
@@ -4,6 +4,11 @@ module SearchCraft
   class Error < StandardError; end
 end
 
+# dependencies of scenic gem without loading all of rails
+require "delegate"
+require "active_support/core_ext/module"
+require "active_record/schema_dumper"
+
 require_relative "searchcraft/version"
 require_relative "searchcraft/builder"
 require_relative "searchcraft/model"
