e333fd44e000911ccb0e000f3ce971f2d916e184
Dr Nic Williams <drnicwilliams@gmail.com>
2023-08-20 18:04:55 +1000

    Use faker product names to derive categories

--- dummy_app/db/seeds.rb
+++ dummy_app/db/seeds.rb
@@ -1,5 +1,4 @@
 num_products = ENV.fetch('NUM_PRODUCTS', 50).to_i
-num_categories = ENV.fetch('NUM_CATEGORIES', 10).to_i
 
 ProductCategory.destroy_all
 Product.destroy_all
@@ -13,17 +12,20 @@ num_products.times do |i|
   )
 end
 
-puts "Creating #{num_categories} categories..."
-num_categories.times do |i|
+# Products' name ends with their category, e.g. Awesome Paper Knife -> Knife
+category_names = Product.pluck(:name).map { |name| name.split.last }.uniq
+
+puts "Creating #{category_names.count} categories..."
+category_names.each do |name|
   Category.create!(
-    name: Faker::Commerce.department,
+    name: name,
     active: true
   )
 end
 
 puts "Creating product categories..."
 Product.all.each do |product|
-  join_categories = Category.limit(rand(1..num_categories))
+  join_categories = Category.limit(rand(1..category_names.count))
   join_categories.each do |category|
     ProductCategory.create!(
       product: product,
