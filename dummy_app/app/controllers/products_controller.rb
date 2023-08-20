490f27b352c16d2e5661af8803c5472754cbcd1c
Dr Nic Williams <drnicwilliams@gmail.com>
2023-08-21 08:21:29 +1000

    ?searchcraft=true page only supports filter by category; else get duplicates

--- dummy_app/app/controllers/products_controller.rb
+++ dummy_app/app/controllers/products_controller.rb
@@ -2,6 +2,8 @@ class ProductsController < ApplicationController
   before_action :set_currency
 
   def index
+    @categories = Category.order(:name).load
+
     if (@using_searchcraft = params.delete(:searchcraft) == "true")
       return index_by_searchcraft
     end
@@ -14,8 +16,6 @@ class ProductsController < ApplicationController
     end
 
     @products = @products.load
-
-    @categories = Category.order(:name).load
   rescue ActiveRecord::RecordNotFound
     redirect_to root_url
   end
@@ -23,16 +23,20 @@ class ProductsController < ApplicationController
   private
 
   def index_by_searchcraft
+    @categories = Category.order(:name).load
+
     @products = ProductSearch.all
 
-    if (category_id = params.delete(:category_id))
-      @category = Category.find(category_id)
-      @products = @products.within_category(@category)
+    unless (category_id = params.delete(:category_id))
+      # get first category and redirect to filter by it
+      category_id = @categories.first.id
+      return redirect_to root_url(searchcraft: true, category_id: category_id)
     end
 
-    @products = @products.load
+    @category = Category.find(category_id)
+    @products = @products.within_category(@category)
 
-    @categories = Category.order(:name).load
+    @products = @products.load
   end
 
   def set_currency
