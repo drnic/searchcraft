d6dbad54c3c02fd63b292896f36b6d79d39ad046
Dr Nic Williams <drnicwilliams@gmail.com>
2023-08-22 09:14:33 +1000

    Fix system test

--- dummy_app/test/system/dashboards_test.rb
+++ dummy_app/test/system/dashboards_test.rb
@@ -4,14 +4,14 @@ class DashboardsTest < ApplicationSystemTestCase
   test "visiting the dashboards" do
     visit root_url
 
-    assert_selector "h1", text: "Biggest Discounts"
+    assert_selector "h2", text: "Biggest Discounts"
     assert_selector "h1", text: "Categories"
     assert_selector "h1", text: "Products"
     assert_selector "h1", text: "ProductSearches", count: 0
 
     click_on "SearchCraft"
 
-    assert_selector "h1", text: "Biggest Discounts"
+    assert_selector "h2", text: "Biggest Discounts"
     assert_selector "h1", text: "Categories"
     assert_selector "h1", text: "Products", count: 0
     assert_selector "h1", text: "ProductSearches"
