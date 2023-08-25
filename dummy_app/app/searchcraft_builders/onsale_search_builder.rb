1979c3dda2959ec04292a75a6ae5298990ac725e
Dr Nic Williams <drnicwilliams@gmail.com>
2023-08-25 11:17:12 +1000

    ProductSearch/OnsaleSearch calculate ratings

--- dummy_app/app/searchcraft_builders/onsale_search_builder.rb
+++ dummy_app/app/searchcraft_builders/onsale_search_builder.rb
@@ -14,7 +14,13 @@ class OnsaleSearchBuilder < SearchCraft::Builder
         "product_prices.sale_price AS sale_price, " \
         "product_prices.sale_price AS price, " \
         "product_prices.currency AS currency, " \
-        "CAST(ROUND((1 - (1.0 * product_prices.sale_price / product_prices.base_price)) * 100) AS integer) AS discount_percent"
+        "CAST(ROUND((1 - (1.0 * product_prices.sale_price / product_prices.base_price)) * 100) AS integer) AS discount_percent", \
+        "(SELECT COUNT(*) FROM product_reviews WHERE product_reviews.product_id = products.id) AS reviews_count", \
+        "(SELECT AVG(rating) FROM product_reviews WHERE product_reviews.product_id = products.id) AS reviews_average", \
+        # Count reviews by unique customers
+        "(SELECT COUNT(DISTINCT product_reviews.customer_id) FROM product_reviews WHERE product_reviews.product_id = products.id) AS customer_reviews_count", \
+        # Find the latest review for each customer, and average their rating
+        "(SELECT AVG(rating) FROM (SELECT DISTINCT ON (customer_id) rating FROM product_reviews WHERE product_reviews.product_id = products.id ORDER BY customer_id, created_at DESC) AS latest_reviews) AS average_review_for_latest"
       )
   end
 end
--- dummy_app/app/searchcraft_builders/product_search_builder.rb
+++ dummy_app/app/searchcraft_builders/product_search_builder.rb
@@ -13,7 +13,13 @@ class ProductSearchBuilder < SearchCraft::Builder
         "product_prices.base_price AS base_price, " \
         "product_prices.sale_price AS sale_price, " \
         "product_prices.currency AS currency, " \
-        "COALESCE(product_prices.sale_price, product_prices.base_price) AS price"
+        "COALESCE(product_prices.sale_price, product_prices.base_price) AS price", \
+        "(SELECT COUNT(*) FROM product_reviews WHERE product_reviews.product_id = products.id) AS reviews_count", \
+        "(SELECT AVG(rating) FROM product_reviews WHERE product_reviews.product_id = products.id) AS reviews_average", \
+        # Count reviews by unique customers
+        "(SELECT COUNT(DISTINCT product_reviews.customer_id) FROM product_reviews WHERE product_reviews.product_id = products.id) AS customer_reviews_count", \
+        # Find the latest review for each customer, and average their rating
+        "(SELECT AVG(rating) FROM (SELECT DISTINCT ON (customer_id) rating FROM product_reviews WHERE product_reviews.product_id = products.id ORDER BY customer_id, created_at DESC) AS latest_reviews) AS average_review_for_latest"
       )
   end
 
