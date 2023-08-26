32d4ec56318ba4464150c4a8e6f8de22371879bf
Dr Nic Williams <drnicwilliams@gmail.com>
2023-08-26 14:58:50 +1000

    Make sure Rake::Task is loaded

--- lib/searchcraft/annotate.rb
+++ lib/searchcraft/annotate.rb
@@ -2,9 +2,13 @@ module SearchCraft::Annotate
   extend ActiveSupport::Concern
 
   included do
+    # If using annotate gem, then automatically annotate models after rebuilding views
     def annotate_models!
       return unless Object.const_defined?(:Annotate)
+
+      require "rake/task" unless Object.const_defined?("Rake::Task")
       Annotate.load_tasks unless Rake::Task[:annotate_models]
+
       Rake::Task[:annotate_models].invoke
     end
   end
