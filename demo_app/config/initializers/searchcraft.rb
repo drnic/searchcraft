2116479a9edab1b3b9203000c806c37aa76e2e80
Dr Nic Williams <drnicwilliams@gmail.com>
2023-09-07 10:30:49 +1000

    Railtie can avoid autobuilding via config

--- demo_app/config/initializers/searchcraft.rb
+++ demo_app/config/initializers/searchcraft.rb
@@ -2,6 +2,8 @@ SearchCraft.configure do |config|
   # Set debug=true to show progress + errors
   config.debug = ENV["SEARCHCRAFT_DEBUG"] == "true"
 
+  config.disable_autorebuild = Rails.env.production?
+
   # If autodiscovery does not work for you, you can explicitly list the
   # builder classes you want to use.
   #
--- lib/searchcraft/railtie.rb
+++ lib/searchcraft/railtie.rb
@@ -5,6 +5,7 @@ module SearchCraft
     initializer "searchcraft.reloader_hook" do
       ActiveSupport::Reloader.to_prepare do
         next unless SearchCraft.database_ready?
+        next if SearchCraft.config.disable_autorebuild
 
         warn "[#{Rails.env}] running: SearchCraft::Builder.rebuild_any_if_changed!" if SearchCraft.debug?
 
